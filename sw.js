39b52305094c886828a628cd7ae7615cb4a88b43
VOLTAGE_LORD <officialvoltinstasnap@gmail.com>
2025-04-06 11:03:33 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,38 @@
+const CACHE_NAME = 'volt-tts-v2';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap',
+  'https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js'
+];
 
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
